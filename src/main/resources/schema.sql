5bc0116000027625775152fa582c9ed7ab754655
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-12-04 18:15:34 +0530

    Implement CreateNewLecturer database part

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS lecture
+CREATE TABLE IF NOT EXISTS lecturer
 (
     id             INT AUTO_INCREMENT PRIMARY KEY,
     name           VARCHAR(250) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS full_time_rank
     lecturer_id INT NOT NULL,
     `rank`      INT NOT NULL,
     CONSTRAINT pk_full PRIMARY KEY (lecturer_id, `rank`),
-    CONSTRAINT fk_full FOREIGN KEY (lecturer_id) REFERENCES lecture (id)
+    CONSTRAINT fk_full FOREIGN KEY (lecturer_id) REFERENCES lecturer (id)
 );
 
 CREATE TABLE IF NOT EXISTS part_time_rank
@@ -22,5 +22,5 @@ CREATE TABLE IF NOT EXISTS part_time_rank
     lecturer_id INT NOT NULL,
     `rank`      INT NOT NULL,
     CONSTRAINT pk_part PRIMARY KEY (lecturer_id, `rank`),
-    CONSTRAINT fk_part FOREIGN KEY (lecturer_id) REFERENCES lecture (id)
+    CONSTRAINT fk_part FOREIGN KEY (lecturer_id) REFERENCES lecturer (id)
 );
