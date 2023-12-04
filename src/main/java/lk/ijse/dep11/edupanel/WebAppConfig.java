c7bd139eaabffe0eb2aa5bca94a0b783568801c2
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-12-04 17:16:37 +0530

    Create custom validation constraint

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,11 +1,25 @@
 package lk.ijse.dep11.edupanel;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig {
+
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver(){
+//        return new CommonsMultipartResolver();
+//    }
+    @Bean
+    public StandardServletMultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+
 }
